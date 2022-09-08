a2ee21baa4e261793ec6f7732eafefb41da1b6f3
Ilja Murasko <ilj.mur@gmail.com>
2022-09-08 23:06:35 +0300

    update footer links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -121,7 +121,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © 2022 ActiList`,
       },
       prism: {
         theme: lightCodeTheme,
